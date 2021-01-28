c3a49604b1f00cafe62df089b08a07af5df8d0d7
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2021-01-28 18:35:02 +0700

    work on v-model

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <br>
+    <div class="newCat">
+      <h3>Add a Cat</h3>
+      <div><span>Title: </span><input type="text" v-model="newCat.title"></div>
+      <div><span>Message: </span><input type="text" v-model="newCat.msg"></div>
+      <div><span>Link: </span><input type="text" v-model="newCat.link"></div>
+      <div><span>Image URL: </span><input type="text" v-model="newCat.image"></div>
+      
+      <cat-card
+        :cat="newCat"
+      />
+    </div>
+
+    
     <cat-card 
-      v-for="item in ListOfCats" 
-      v-bind:key="item.id" 
+      v-for="(item, index) in ListOfCats" 
+      v-bind:key="index" 
       :cat="item"
     />
   </div>
@@ -16,34 +30,41 @@ export default {
   name: 'App',
   data: function(){
     return {
+      newCat:{
+          title: "Hello Cats",
+          msg: "Hello Cats",
+          link: "www.cat.com",
+          visible: true,
+          image: "https://i.imgur.com/Ss75Vfa.jpg"
+      },
       ListOfCats: [
         {
-          id: 1,
           title: "Cats",
           msg: "Hello Cats",
           link: "www.cat.com",
-          visible: true
+          visible: true,
+          image: "https://i.pinimg.com/736x/07/6b/9c/076b9ca43b2521b7990fa0a85b309e73.jpg"
         },
         {
-          id: 2,
           title: "Big Cats",
           msg: "Hello Big Cats",
           link: "www.bigcat.com",
-          visible: true
+          visible: true,
+          image: "https://i.imgur.com/Ss75Vfa.jpg"
         },
         {
-          id: 3,
           title: "Bigger Cats",
           msg: "Hello Bigger Cats",
           link: "www.biggercat.com",
-          visible: false
+          visible: true,
+          image: "https://thumbs.dreamstime.com/z/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg"
         },
         {
-          id: 4,
           title: "The Bigest Cats",
           msg: "Hello Biggest Cats",
           link: "www.biggestcat.com",
-          visible: true
+          visible: true,
+          image: "https://ic.pics.livejournal.com/ibigdan/8161099/7140450/7140450_original.jpg"
         },
       ]  
     }
@@ -62,5 +83,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.newCat{
+  width: 300px;
+  padding: 10px;
+  margin: 0 auto;
 }
 </style>
--- src/components/catCard.vue
+++ src/components/catCard.vue
@@ -1,16 +1,20 @@
 <template>
-  <div>
-    <hr>
-    <div v-if="cat.visible == true" >
-      <h2>{{cat.id}}</h2>
-        <h1>{{cat.title}}</h1>
-        <p>{{cat.msg}}</p>
-        <p>{{cat.link}}</p>
-    </div>
-    <div v-else>
-       <strong>{{cat.id}} This '{{cat.title}}' is not available.</strong> 
-    </div>
-  </div>
+  <a :href="cat.link" v-if="cat.title">
+      <div :class="[cat.visible ? 'visible' : 'invisible','catCard']">
+        <div v-if="cat.visible == true" >
+          <h2>{{cat.id}}</h2>
+          <img v-if="cat.image" class="catImage" :src="cat.image" alt="">
+            <h1>{{cat.title}}</h1>
+            <p>{{cat.msg}}</p>
+            <p>{{cat.link}}</p>
+        </div>
+        <div v-else>
+          <h2>{{cat.id}}</h2>
+          <img class="catImage" :src="cat.image" alt="">
+            <h1>{{cat.title}}</h1>
+        </div>
+      </div>
+  </a>
 
 </template>
 
@@ -25,18 +29,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.catCard{
+  display: inline-flex;
+  border: 1px solid #e6e6e6;
+  width: 300px;
+  height: 300px;
+  justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.visible{
+  background: green;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.invisible{
+ background: grey;
 }
 a {
-  color: #42b983;
+  text-decoration: none;
+  color:whitesmoke
+}
+.catImage {
+  width: 100px;
+  height: 100px;
 }
 </style>
